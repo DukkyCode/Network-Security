5b57b0579fe430b1aebf25bc99016ad0f6c1e3f9
Duc Nguyen <ducanhnguyen_official@outlook.com>
2023-02-08 17:07:49 -0600

    Homework 1 part I finish

--- HW1p1/unencryptedim.py
+++ HW1p1/unencryptedim.py
@@ -11,13 +11,16 @@ class SetupServer:
         self.host = 'localhost'
         self.port = 9999
 
+	    #Terminate Signal Handler
+        signal.signal(signal.SIGINT, self.handler)
+
         # Bind thee socket to the port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((self.host, self.port))
 
         # Listen for incoming
-        self.server.listen(5)
+        self.server.listen(1)
         # Read and Write File Descriptor
         self.Rd = [self.server, sys.stdin]
         self.Wd = []
@@ -30,21 +33,26 @@ class SetupServer:
                 if s is self.server:
                     # Accepting connection from client
                     conn, addr = s.accept()
-                    conn.setblocking(0)
                     self.Rd.append(conn)
 
                 # If s is standard input
                 elif s is sys.stdin:
-                    msg = sys.stdin.readline()
+                    msg = s.readline()
                     if msg == "":
                         break
-                    self.server.sendall(msg.encode('utf-8'))
+                    
+                    #If there is no connection of client send error
+                    if conn in self.Rd:
+                        conn.sendall(msg.encode('utf-8'))
+                    else:
+                        print("No client connection")
+                        break
 
                 else:
-                    data = self.server.recv(1024)
+                    data = s.recv(1024)
                     if data:
                         data = data.decode('utf-8')
-                        sys.stdout.write(data + '\n')
+                        sys.stdout.write(data)
                         sys.stdout.flush()
 
                         # if the connection is not in Write file description, then append it into the list
@@ -55,14 +63,20 @@ class SetupServer:
                         if s in self.Wd:
                             self.Wd.remove(s)
                         self.Rd.remove(s)
-                        self.server.close()
+                        s.close()
 
+    def handler(self,sig,frame):
+        sys.exit(0)
+        self.server.close()
 
 class SetupClient:
     def __init__(self, host):
         # Set up host and local port
         self.host = host
         self.port = 9999
+
+	    #Terminate Signal Handler
+        signal.signal(signal.SIGINT, self.handler)
 
         # Bind thee socket to the port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -80,14 +94,21 @@ class SetupClient:
             for s in readable:
                 # If the connection is established
                 if s is self.server:
-                    data = s.recv(1024)
-                    sys.stdout.write(data + '\n')
-                    sys.stdout.flush()
+                    data = s.recv(1024).decode('utf-8')
+                    if data:
+                        sys.stdout.write(data)
+                        sys.stdout.flush()
+                    else:
+                        sys.exit(0)
                 else:
                     msg = s.readline()
                     if msg == "":
                         break
                     self.server.sendall(msg.encode('utf-8'))
+    
+    def handler(self,sig,frame):
+        self.server.close()
+        sys.exit(0)
 
 
 # Main Function
@@ -96,13 +117,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--s", dest='server', default=False,
                         action="store_true", help="server mode", required=False)
-    parser.add_argument("--c", dest='client', type=str,
+    parser.add_argument("--c", dest='destination', type=str,
                         help="client mode", required=False)
     args = parser.parse_args()
 
     if args.server:
         SetupServer()
-    elif args.client:
-        SetupClient()
+    elif args.destination:
+        SetupClient(args.destination)
     else:
-        sys.stdout.write("Please add argument --s and --c 'Destination'")
+        sys.stdout.write("Please add argument --s or --c 'Destination'\n")
+        sys.exit(0)
